019e62ba26a6ff083f3b2c26f949021348abd86e
Arttu Multanen <karethoth@gmail.com>
2021-12-01 22:10:57 +0200

    1-5 Implement repeating-key XOR

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -117,3 +117,21 @@ TEST_CASE("1-4: Detect single-character XOR", "[decrypt_single_char_xor_from_fil
   REQUIRE(current_best.value == "Now that the party is jumping\n");
 }
 
+TEST_CASE("1-5: Implement repeating-key XOR", "[decrypt_single_char_xor_from_file]")
+{
+  REQUIRE(
+    byte_to_hex_string(
+      xor_byte_strings(
+        "Burning 'em, if you ain't quick and nimble\n" \
+        "I go crazy when I hear a cymbal",
+        "ICE"
+      )
+    )
+    ==
+    to_upper(
+      "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272" \
+      "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+    )
+  );
+}
+
