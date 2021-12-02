0cb3354a1f54c8725e402554684185a4e4ba8cf0
Arttu Multanen <karethoth@gmail.com>
2021-12-02 18:55:16 +0200

    Update

--- src/types.h
+++ src/types.h
@@ -7,5 +7,17 @@ namespace minicrypto
   using hex_string    = std::string;
   using byte_string   = std::string;
   using base64_string = std::string;
+
+  template <typename T>
+  struct ValueWithConfidence
+  {
+    T value;
+    float confidence;
+
+    ValueWithConfidence(T value, float confidence)
+    : value(value), confidence(confidence)
+    {
+    }
+  };
 }
 
--- src/utils.h
+++ src/utils.h
@@ -4,14 +4,6 @@
 
 namespace minicrypto
 {
-  struct ValueWithConfidence
-  {
-    byte_string value;
-    float confidence;
-
-    ValueWithConfidence(byte_string result, float confidence);
-  };
-
   uint8_t hexchar_to_nibble(uint8_t c);
   uint8_t nibble_to_hexchar(uint8_t c);
 
@@ -19,6 +11,7 @@ namespace minicrypto
   hex_string byte_to_hex_string(const byte_string &input);
 
   base64_string byte_to_base64_string(const byte_string &input);
+  byte_string   base64_to_byte_string(const base64_string &input);
 
   byte_string xor_byte_string(byte_string input_a, const uint8_t key);
   byte_string xor_byte_strings(byte_string input_a, const byte_string& input_b);
@@ -27,6 +20,10 @@ namespace minicrypto
   size_t hamming_distance(uint8_t a, uint8_t b);
   size_t hamming_distance(const byte_string& input_a, const byte_string& input_b);
 
-  ValueWithConfidence decrypt_single_char_xor(const byte_string& input);
+  ValueWithConfidence<byte_string>
+  decrypt_single_char_xor(const byte_string& input);
+
+  ValueWithConfidence<byte_string>
+  decrypt_repeating_key_xor(const byte_string& input);
 }
 
